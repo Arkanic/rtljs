a2a25a6a978c9679c0c2f7d9b3c2a0dac5616b01
Arkanic <silverfishasaurus@gmail.com>
2021-10-21 02:47:15 +0000

    build for previous commit

--- lib/rtlsdr-device.d.ts
+++ lib/rtlsdr-device.d.ts
@@ -74,4 +74,122 @@ export default class RTLSDRDevice {
      * @returns Correction value in parts per million (ppm)
      */
     getFreqCorrection(): number;
+    /**
+     * Get the tuner type.
+     *
+     * @returns RTLSDR_TUNER_UNKNOWN on error, tuner type otherwise
+     */
+    getTunerType(): string;
+    /**
+     * Get a list of gains supported by the tuner.
+     *
+     * NOTE: The gains argument must be preallocated by the caller. If NULL is
+     * being given instead, the number of available gain values will be returned.
+     *
+     * @param gains Array of gain values. In tenths of a dB, 115 means 11.5 dB.
+     * @returns Number of available (returned) gain values otherwise
+     */
+    getTunerGains(gains: ref.Pointer<number>): number;
+    /**
+     * Set the gain for the device.
+     * Manual gain mode must be enabled for this to work.
+     *
+     * Valid gain values (in tenths of a dB) for the E4000 tuner:
+     * -10, 15, 40, 65, 90, 115, 140, 165, 190,
+     * 215, 240, 290, 340, 420, 430, 450, 470, 490
+     *
+     * Valid gain values may be queried with rtlsdr_get_tuner_gains function.
+     *
+     * @param gain Measured in tenths of a dB, 115 means 11.5 dB.
+     */
+    setTunerGain(gain: number): void;
+    /**
+     * Set the bandwidth for the device.
+     *
+     * @param bw Bandwidth in Hz. Zero means automatic BW selection.
+     */
+    setTunerBandwidth(bw: number): void;
+    /**
+     * Get actual gain the device is configured to.
+     *
+     * @returns Gain in tenths of a dB, 115 means 11.5 dB.
+     */
+    getTunerGain(): number;
+    /**
+     * Set the intermediate frequency gain for the device.
+     *
+     * @param stage Intermediate frequency gain stage number (1 to 6 for E4000)
+     * @param gain Measured in tenths of a dB, -30 means -3.0 dB.
+     */
+    setTunerIfGain(stage: number, gain: number): void;
+    /**
+     * Set the gain mode (automatic/manual) for the device.
+     * Manual gain mode must be enabled for the gain setter function to work.
+     *
+     * @param manual Gain mode, 1 means manual gain mode shall be enabled.
+     */
+    setTunerGainMode(manual: number): void;
+    /**
+     * Set the sample rate for the device, also selects the baseband filters
+     * according to the requested sample rate for tuners where this is possible.
+     *
+     * @param rate samp_rate the sample rate to be set, possible values are:
+     * 		    225001 - 300000 Hz
+     * 		    900001 - 3200000 Hz
+     * 		    sample loss is to be expected for rates > 2400000
+     */
+    setSampleRate(rate: number): void;
+    /**
+     * Get actual sample rate the device is configured to.
+     *
+     * @returns Sample rate in Hz
+     */
+    getSampleRate(): number;
+    /**
+     * Enable test mode that returns an 8 bit counter instead of the samples.
+     * The counter is generated inside the RTL2832.
+     *
+     * @param on Test mode, 1 means enabled, 0 disabled
+     */
+    setTestmode(on: number): void;
+    /**
+     * Enable or disable the internal digital AGC of the RTL2832.
+     *
+     * @param on digital AGC mode, 1 means enabled, 0 disabled
+     */
+    setAGCMode(on: number): void;
+    /**
+     * Enable or disable the direct sampling mode. When enabled, the IF mode
+     * of the RTL2832 is activated, and rtlsdr_set_center_freq() will control
+     * the IF-frequency of the DDC, which can be used to tune from 0 to 28.8 MHz
+     * (xtal frequency of the RTL2832).
+     *
+     * @param on 0 means disabled, 1 I-ADC input enabled, 2 Q-ADC input enabled
+     */
+    setDirectSampling(on: number): void;
+    /**
+     * Get state of the direct sampling mode
+     *
+     * @returns -1 on error, 0 means disabled, 1 I-ADC input enabled
+     *	    2 Q-ADC input enabled
+     */
+    getDirectSampling(): number;
+    /**
+     * Enable or disable offset tuning for zero-IF tuners, which allows to avoid
+     * problems caused by the DC offset of the ADCs and 1/f noise.
+     *
+     * @param on 0 means disabled, 1 enabled
+     */
+    setOffsetTuning(on: number): void;
+    /**
+     * Get state of the offset tuning mode
+     *
+     * @returns -1 on error, 0 means disabled, 1 enabled
+     */
+    getOffsetTuning(): number;
+    /**
+     * Reset the device buffer
+     */
+    resetBuffer(): void;
+    readSync(len: number): any;
 }
