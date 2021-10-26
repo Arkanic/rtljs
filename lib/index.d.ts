4ed1ccb7d9175c3453149ab119c35678d16cd464
Arkanic <silverfishasaurus@gmail.com>
2021-10-26 00:32:19 +0000

    proper documentation

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,4 @@
 import { getDeviceCount, getDeviceName, getDeviceUSBStrings, getIndexBySerial, open, close } from "./rtlsdr-static";
 import { rtlsdr_tunerEnum } from "./baremetal";
-export { getDeviceCount, getDeviceName, getDeviceUSBStrings, getIndexBySerial, open, close, rtlsdr_tunerEnum };
+import RTLSDRDevice from "./rtlsdr-device";
+export { getDeviceCount, getDeviceName, getDeviceUSBStrings, getIndexBySerial, open, close, rtlsdr_tunerEnum, RTLSDRDevice };
