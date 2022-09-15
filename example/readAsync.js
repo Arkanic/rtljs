5bfc16c26345bc6f7f6df3bb3b516c6c7c36da89
Arkanic <50847107+Arkanic@users.noreply.github.com>
2022-09-15 18:35:50 +1200

    whoops

--- example/readAsync.js
+++ example/readAsync.js
@@ -4,6 +4,8 @@ const device = rtljs.open(0);
 
 device.setCenterFreq(1090 * rtljs.mhz);
 
+device.resetBuffer();
+
 device.readAsync((buf, len, ctx) => {
     console.log(JSON.stringify(buf));
 }, 0, 512);
